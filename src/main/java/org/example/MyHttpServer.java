2c6f446d3ce6c166ce0f7ccfd99f7b7751236bdc
siddharth <siddharth@LAPTOP-32JQKCS6.localdomain>
2025-06-19 17:22:43 -0500

    basic server

---
+++ src/main/java/org/example/MyHttpServer.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class MyHttpServer {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // sets a default executor
+        server.start();
+        System.out.println("Server started on port 8000...");
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "This is the response";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
